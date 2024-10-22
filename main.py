a4347fe711d3ba594d735ed77fc5fe7525e7b350
Istvan Berko <istvan.berko@gmail.com>
2024-10-22 15:04:21 -0500

    completed the task

---
+++ main.py
@@ -0,0 +1,34 @@
+
+
+def main():
+    path_to_file = "/Users/istvan.berko/workspace/github.com/istvanberko/bookbot/books/frankenstein.txt"
+    with open(path_to_file) as f:
+        file_content = f.read()
+    print_character_values(count_characters(file_content))
+
+
+def count_words(fcontent):
+    words = fcontent.split()
+    return len(words)
+
+def count_characters(fcontent):
+    character_count = {}
+
+    fcontent = fcontent.lower()
+    
+
+    for char in fcontent:
+        if char in character_count and char.isalpha():
+            character_count[char] += 1
+        elif char.isalpha():
+            character_count[char] = 1
+
+    character_count = dict(sorted(character_count.items(), key=lambda item: item[1], reverse=True))
+    
+    return character_count
+
+def print_character_values(ccount):
+    for i in ccount:
+        print(f"The '{i}' character was found {ccount[i]} times")
+
+main()
